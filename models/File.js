f9de6dd1eeb9101c6b3559c7de1b3f0f98e94808
Rohochyi <oxid1109@gmail.com>
2024-10-31 15:13:16 +0200

    Implement main interfaces.
     Create UI

--- models/File.js
+++ models/File.js
@@ -1,10 +1,16 @@
 /**
  * @property {string} filename
  * @property {string} filepath
+ * @property {number} size
  * @property {Date} createdAt
  */
 class File {
-
+    constructor(filename = '', filepath = '', size = 0, createdAt = new Date()) {
+        this.filename = filename;
+        this.filepath = filepath;
+        this.size = size;
+        this.createdAt = createdAt;
+    }
 }
 
 module.exports = File;
--- services/UserService.js
+++ services/UserService.js
@@ -1,8 +1,11 @@
+const fs = require('fs');
+const path = require('path');
 const User = require('../models/User');
 
 class UserService {
     constructor() {
-
+        this.usersFile = path.join(__dirname, '..', 'runtime', 'users.json');
+        this.users = this.loadUsers();
     }
 
     /**
@@ -13,23 +16,65 @@ class UserService {
      * @returns {boolean}
      */
     createUser(username, email, password) {
+        if (this.users.some(user => user.email === email)) {
+            return false;
+        }
 
+        const newUser = new User();
+        newUser.username = username;
+        newUser.email = email;
+        newUser.password = this.hashPassword(password);
+        newUser.roles = ['user'];
+        newUser.createdAt = new Date();
+
+        this.users.push(newUser);
+        this.saveUsers();
+        return true;
     }
 
     /**
      * @param {string} email
      * @param {string} password
-     * @returns {boolean}
+     * @returns {User|null}
      */
     login(email, password) {
-
+        const user = this.users.find(user => user.email === email);
+        if(user && this.verifyPassword(password, user.password)) {
+            return user;
+        }
+        return null;
     }
 
     /**
-     * @returns void
+     * @param {string} password
+     * @returns {string}
      */
-    logout() {
+    hashPassword(password) {
+        return `hashed_${password}`;
+    }
 
+    /**
+     * @param {string} password
+     * @param {string} hashedPassword
+     * @returns {boolean}
+     */
+    verifyPassword(password, hashedPassword) {
+        return this.hashPassword(password) === hashedPassword;
+    }
+
+    /**
+     * @returns {User[]}
+     */
+    loadUsers() {
+        if (fs.existsSync(this.usersFile)) {
+            const data = fs.readFileSync(this.usersFile).toString();
+            return JSON.parse(data);
+        }
+        return [];
+    }
+
+    saveUsers() {
+        fs.writeFileSync(this.usersFile, JSON.stringify(this.users, null, 2));
     }
 }
 
