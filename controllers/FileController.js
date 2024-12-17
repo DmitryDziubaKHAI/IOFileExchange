cfcd5d518d504fd7cf97b4e1aebc51da6898078d
Rohochyi <y.v.rohochyi@student.csn.khai.edu>
2024-12-17 21:59:33 +0200

    Change files list view by user

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -16,7 +16,10 @@ class FileController {
      */
     listFiles(req, res) {
         const files = fileService.getFilesListByUserId(req.session.user.id);
-        res.status(200).json(files);
+        res.status(200).json(files.map(file => {
+            file.downloadPage = '/download/' + file.id;
+            return file;
+        }));
     }
 
     /**
@@ -24,13 +27,42 @@ class FileController {
      * @param {import('express').Response} res
      */
     async downloadFile(req, res) {
-        const {id, key} = req.body;
+        const {id, key} = req.params;
         const file = fileService.getFileById(id);
-        if(!file || !(await file.checkDownloadTmpKey(key, req.session.userId))) {
-            res.status(404).json({success: false});
+        if(!file || !(await file.checkDownloadTmpKey(key, req.session.user.id))) {
+            res.status(404).send('Not found 404');
             return;
         }
         res.download(file.filepath);
+    }
+
+    /**
+     * @param {import('express').Request & {session: object}} req
+     * @param {import('express').Response} res
+     */
+    async downloadPage(req, res) {
+        const id = req.params.id;
+        const file = fileService.getFileById(id);
+        if(!file) {
+            res.status(404).send('Not found 404');
+            return;
+        }
+        let isPasswordInvalid = false;
+        if(req.method === 'POST') {
+            const {password} = req.body;
+            if(await file.checkPassword(password)) {
+                const key = file.getDownloadTmpKey(req.session.user.id);
+                res.redirect(`/download/${file.id}/${key}`);
+                return;
+            } else {
+                isPasswordInvalid = true;
+            }
+        }
+        res.render('layout', {
+            content: 'downloadPage',
+            file: file,
+            isPasswordInvalid: isPasswordInvalid
+        });
     }
 }
 
